45eeb2c72145e1b4d273151b6df53d15a48a607f
nazari2079 <mohammadnazary2000@gmail.com>
2023-12-27 19:37:04 +0330

    change vite config for library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/app.tsx",
+      name: "react-simple-folder-tree",
+      fileName: (format) => `react-simple-folder-tree.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+});
