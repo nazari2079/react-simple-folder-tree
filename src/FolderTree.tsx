a36252b31e3ffb2ac9a927429f0db1c38300107a
nazari2079 <mohammadnazary2000@gmail.com>
2023-12-27 19:46:39 +0330

    change app name to folder tree

--- src/FolderTree.tsx
+++ src/FolderTree.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import Folder from "./components/Folder";
 import { FoldersProvider } from "./context/folderDataContext";
 
-const App: FC = () => {
+const FolderTree: FC = () => {
   return (
     <div className="p-10 max-w-[500px]">
       <FoldersProvider>
@@ -12,4 +12,4 @@ const App: FC = () => {
   );
 };
 
-export default App;
+export default FolderTree;
