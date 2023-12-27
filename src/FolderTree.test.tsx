4b5afe18fa8bc5c4003e2061b93f77428b2bb128
nazari2079 <mohammadnazary2000@gmail.com>
2023-12-27 21:24:13 +0330

    add test

---
+++ src/FolderTree.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { describe, expect, it } from "vitest";
+import FolderTree from "./FolderTree";
+import foldersInitialData from "./data/foldersInitialData.json";
+
+describe("FolderTree component", () => {
+  it("FolderTree component renders correctly with data", () => {
+    const component = renderer.create(<FolderTree data={foldersInitialData} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
